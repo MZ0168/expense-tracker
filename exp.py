628c6aaf885650307015e5e73bccb7fe30602a67
MZ0168 <rayyanzayed12@gmail.com>
2025-05-03 12:29:44 -0400

    exp.py

--- exp.py
+++ exp.py
@@ -46,6 +46,7 @@ st.write(f"${total_expenses(st.session_state.expenses)}")
 st.number_input("Set your Budget")
 st.success()
 st.error()
+
 # Filter by category
 st.subheader("Filter by Category")
 categories = list(set([e['category'] for e in st.session_state.expenses]))
